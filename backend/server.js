aa3ea91c86dc6c96ae36828e7ca43f6abf6cc7f5
Ngawang Tashi Moktan <45892775+ngawang-tashi1986@users.noreply.github.com>
2025-07-12 13:35:22 +0800

    4 post routes and controller

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,17 @@ import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js"
 import cookieParser from "cookie-parser";
+import { v2 as Cloudinary} from "cloudinary";
+import postRoutes from "./routes/post.routes.js";
 
 dotenv.config();
+// Configure Cloudinary
+Cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+// Importing routes
 const app = express();
 const PORT = process.env.PORT || 8000;
 
@@ -15,6 +24,7 @@ app.use(cookieParser()); //to parse cookies from the request
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes); // Uncomment this line if you have post routes
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
